9e5ec1808d3a039caab6da4e56b9279d975882ce
dave <dxmann73@gmail.com>
2018-08-18 17:28:01 +0200

    fix: Enable manual --rm for appserver images

--- wildfly-standalone-demo/buildAndRun.sh
+++ wildfly-standalone-demo/buildAndRun.sh
@@ -2,4 +2,4 @@
 sh build.sh
 
 docker rm -f wildfly-standalone-demo || true \
-    && docker run -it --rm -p 8080:8080 -p 4848:4848 -p 8787:8787 --name wildfly-standalone-demo dxmann73/wildfly-standalone-demo
+    && docker run -it -p 8080:8080 -p 4848:4848 -p 8787:8787 --name wildfly-standalone-demo dxmann73/wildfly-standalone-demo
