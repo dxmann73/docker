440080710659fc710a345caf0833550a6fe726fb
dave <dxmann73@gmail.com>
2018-08-19 10:23:40 +0200

    feat: Add standalone container to docker_kafkanet

--- wildfly-kafka-streams-demo/buildAndRun.sh
+++ wildfly-kafka-streams-demo/buildAndRun.sh
@@ -2,4 +2,7 @@
 sh build.sh
 
 docker rm -f wildfly-kafka-streams-demo || true \
-    && docker run -it -p 8080:8080 -p 4848:4848 -p 8787:8787 --name wildfly-kafka-streams-demo dxmann73/wildfly-kafka-streams-demo
+    && docker run -it \
+    -p 8080:8080 -p 4848:4848 -p 8787:8787 \
+    --network="docker_kafkanet" \
+    --name wildfly-kafka-streams-demo dxmann73/wildfly-kafka-streams-demo
