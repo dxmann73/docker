0e0835ebea077c9a28d984dc136fa362b8074a1f
dave <dxmann73@gmail.com>
2018-08-19 10:46:32 +0200

    feat: Add wildfly-kafka-streams to cluster; move standalone ports one up

--- wildfly-kafka-streams-demo/buildAndRun.sh
+++ wildfly-kafka-streams-demo/buildAndRun.sh
@@ -3,6 +3,6 @@ sh build.sh
 
 docker rm -f wildfly-kafka-streams-demo || true \
     && docker run -it \
-    -p 8080:8080 -p 4848:4848 -p 8787:8787 \
+    -p 8081:8080 -p 4849:4848 -p 8788:8787 \
     --network="docker_kafkanet" \
     --name wildfly-kafka-streams-demo dxmann73/wildfly-kafka-streams-demo
